86bd33ae43ce23b8cec936751e9d6b062f8b4f53
LuizGarbini <garbasneto@gmail.com>
2025-03-20 23:27:26 -0300

    Obtendo parâmetros das rotas

--- src/controllers/tickets/index.js
+++ src/controllers/tickets/index.js
@@ -7,6 +7,9 @@
  */
 
 export function index({ request, response, database }) {
+  const { status } = request.query;
+  console.log(status);
+
   const tickets = database.select("tickets");
 
   return response.end(JSON.stringify(tickets));
